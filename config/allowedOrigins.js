08a18fd89ac00e252ee4480279b74deb4500e148
German L rojas <115124911+germanlopr@users.noreply.github.com>
2023-12-12 15:42:47 -0500

    Update allowedOrigins.js
    changes to new deployment

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,7 @@ const allowedOrigins = [
    // 'http://localhost:3000',
     //'http://localhost:5173',
   //  'https://technotes-opwxjw1jq-gregdorians-projects.vercel.app/',
-    'https://technotes-hyq7.onrender.com/'
+    'https://technotes-api-6yrp.onrender.com/'
    
 ]
 
