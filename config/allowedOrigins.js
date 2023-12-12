76564faf38acfcfad50708c2697f7687237a67f7
gregdorian <greg_dorian@yahoo.com>
2023-12-12 13:22:24 -0500

    ready to deploy the API

---
+++ config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins = [
+   // 'http://localhost:3000',
+    //'http://localhost:5173',
+  //  'https://technotes-opwxjw1jq-gregdorians-projects.vercel.app/',
+    'https://technotes-hyq7.onrender.com/'
+   
+]
+
+module.exports = allowedOrigins
